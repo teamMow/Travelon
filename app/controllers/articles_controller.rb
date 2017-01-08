c4b564fad9857ac8379050cdbece42ddb2ff6bbf
RinaWatanabe <nabee.wata@gmail.com>
2017-01-08 04:32:42 +0000

    article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,15 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only:[:show, :edit, :update, :destroy]
 
   def new
   end
 
   def create
-  	@article =Article.new
-  	@article.title = params[:title]
-  	@article.content =params[:content]
+  	@article = Article.new(article_params)
+  	# @article = Article.new(title: params[:title], content: params[:content])
+  	# @article = Article.new
+  	# @article.title = params[:title]
+  	# @article.content =params[:content]
   	@article.save
   	redirect_to article_path(@article.id)
   end
@@ -16,7 +19,30 @@ class ArticlesController < ApplicationController
   end
 
   def show
-  	@article = Article.find(params[:id])
-  end  	
+  end
+
+  def edit
+  end
+
+  def update
+  	@article.title = params[:title]
+  	@article.content = params[:content]
+  	@article.save
+  	redirect_to article_path(@article.id)
+  end
+
+  def destroy
+  	@article.destroy
+  	redirect_to articles_path
+  end
+
+  private
+  	def set_article
+  		@article = Article.find(params[:id])
+  	end
+
+  	def article_params
+  		params.require(:article).permit(:title, :content)
+  	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+# Articleコントローラ
   get 'articles/new'
-  post '/articles' => 'articles#create'
-  get '/articles' => 'articles#index'
+  # post '/articles' => 'articles#create'
+  # get '/articles' => 'articles#index'
   get '/articles/:id' => 'articles#show', as: 'article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/article/:id' => 'articles#destroy', as: 'destroy_article'
+
+  resources :articles
 
 
+# homeコントローラ
   # get 'home/top'
   root 'home#top'
 
