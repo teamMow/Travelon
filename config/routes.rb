1b86ab56a69145a97470f42ea9437fcff7ead1d0
RinaWatanabe <nabee.wata@gmail.com>
2017-01-14 02:46:45 +0000

    edit update

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   root 'home#top'
 
 
+  # デバイス用のルーティングを自動生成
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
