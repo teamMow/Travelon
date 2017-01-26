b2fba72ba73ebf0025c6c2468e5168ff23319062
watanaberina <nabee.wata@gmail.com>
2017-01-26 06:22:25 +0000

    add model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,16 @@
 class Article < ApplicationRecord
 	validates :title, presence: true
 	validates :content, presence: true
-	# validates :area, presence: :true
+	validates :area, presence: :true
 
 	belongs_to :user
 
 	mount_uploader :img, ImageUploader
 
+	has_many :likes, dependent: :destroy
 
+	# いいねしているかどうか？
+	def liked_by? user
+		likes.where(user_id: user.id).exists?
+	end
 end
