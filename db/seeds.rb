48eab7bf337c2a59ce1a986f58bf149b24dd8100
watanaberina <nabee.wata@gmail.com>
2017-02-19 07:57:42 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(doya: "0")
+Article.create(doya: "1")
+Article.create(doya: "2")
+Article.create(doya: "3")
+Article.create(doya: "4")
+Article.create(doya: "5")
+
+Hobby.create(body: "旅行")
+Hobby.create(body: "スポーツ")
+Hobby.create(body: "映画")
+Hobby.create(body: "音楽")
+Hobby.create(body: "料理")
+Hobby.create(body: "読書")
+Hobby.create(body: "アウトドア")
+Hobby.create(body: "ドライブ")
+Hobby.create(body: "ファッション")
+Hobby.create(body: "ゲーム")
+Hobby.create(body: "ランニング")
+Hobby.create(body: "散歩")
+
