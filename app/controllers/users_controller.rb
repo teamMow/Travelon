a3dcb971fbcbdc343b002383e4cbf5eb1d2c1238
watanaberina <nabee.wata@gmail.com>
2017-04-11 05:02:41 +0000

    modify order of users and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,17 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user.name = params[:user][:name]
-		@user.birthplace = params[:user][:birthplace]
-		@user.sex = params[:user][:sex]
-		@user.age = params[:user][:age]
-		@user.save
+		@user.update(user_params)
+		# @user.name = params[:user][:name]
+		# @user.birthplace = params[:user][:birthplace]
+		# @user.sex = params[:user][:sex]
+		# @user.age = params[:user][:age]
+		# @user.save
 		redirect_to user_path(@user.id)
 	end
 
 	def index
-		@users = User.page(params[:page]).per(5)
+		@users = User.page(params[:page]).per(6).order(created_at: :desc)
 	end
 
 	def destroy
@@ -43,6 +44,10 @@ class UsersController < ApplicationController
 	private
 		def set_user
 			@user = User.find(params[:id])#.includes(:articles)
+		end
+
+		def user_params
+			params.require(:user).permit(:name, :birthplace, :sex, :age)
 		end
 
 end
